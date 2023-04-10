b0433d50d70dffff05c8fd9092dbc23daf5c1ac2
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-10 18:24:13 +0200

    meme generator

--- section-3/meme-generator/src/components/Meme.js
+++ section-3/meme-generator/src/components/Meme.js
@@ -1,11 +1,17 @@
+import React from 'react'
 import memesData from '../memesData'
 
 export default function Meme() {
+
+    const [memeImage, setMemeImg] = React.useState('');
+
     const getMemeImage = () => {
         const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
         const url = memesArray[randomNumber].url;
-        console.log(url);
+        
+        setMemeImg(() => url);
+        console.log(memeImage);
 
     }
 
@@ -13,19 +19,24 @@ export default function Meme() {
         <div id="meme">
             <form>
             </form>
-                <div id="meme--inputs">
-                    <input 
-                        type="text" 
-                        className="form--input"
-                        placeholder="Top text" 
-                    />
-                    <input 
-                        type="text" 
-                        className="form--input"
-                        placeholder="Bottom text"
-                    />
-                </div>
-                <button onClick={getMemeImage} >Get a new meme image</button>
+            <div id="meme--inputs">
+                <input 
+                    type="text" 
+                    className="form--input"
+                    placeholder="Top text" 
+                />
+                <input 
+                    type="text" 
+                    className="form--input"
+                    placeholder="Bottom text"
+                />
+            </div>
+            <button onClick={getMemeImage} >Get a new meme image</button>
+            <img 
+                src={memeImage}
+                width={477}
+                alt="meme"
+            />
         </div>
     )
 }
