08483fad5c092de3e18344bca718ca38fed3419b
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-13 23:41:05 +0200

    completed meme generator

---
+++ section-3/meme-generator2/src/pages/components/Header.js
@@ -0,0 +1,18 @@
+
+export default function Header() {
+    return (
+        <div id="header">
+            <div id="memeface">
+                <img
+                    src="troll-face.png"
+                    alt="troll face"
+                    height="26px"
+                />
+                <h1>Meme Generator</h1>
+            </div>
+            <div id="course">
+                <p>React Course - Project 3</p>
+            </div>
+        </div>
+    )
+}
---
+++ section-3/meme-generator2/src/pages/components/Meme.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export default function Meme() {
+    const [allMemeImages, setAllMemeImages] = React.useState()
+    
+    const randomMemeUrl = () => {
+        if(allMemeImages) {
+            const memesArray = allMemeImages.data.memes;
+            const randomNumber = Math.floor(Math.random() * memesArray.length);
+            const url = memesArray[randomNumber].url;
+            
+            return url;
+        } else {
+            return 'https://i.imgflip.com/26am.jpg'
+        }
+    }
+
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImage: randomMemeUrl()
+        }
+    );
+
+    const getMemeImage = () => {        
+        setMeme((prevMeme) => {
+            return {
+                ...prevMeme,
+                randomImage: randomMemeUrl()
+            }
+        })
+    }
+
+    const handleOnChange = event => {
+        const {name, value} = event.target
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://api.imgflip.com/get_memes');
+            const jsonData = await response.json();
+            setAllMemeImages(() => jsonData);
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        <div id="meme">
+            <form>
+                <div id="meme--inputs">
+                    <input 
+                        type="text" 
+                        className="form--input"
+                        placeholder="Top text"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleOnChange}
+                        />
+                    <input 
+                        type="text" 
+                        className="form--input"
+                        placeholder="Bottom text"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleOnChange}
+                        />
+                </div>
+            </form>
+            <button onClick={getMemeImage} >Get a new meme image</button>
+            <div className='meme-img'>
+                <img 
+                    src={meme.randomImage}
+                    alt="meme"
+                />
+                <h2 className='meme--text top-text'>{meme.topText}</h2>
+                <h2 className='meme--text bottom-text'>{meme.bottomText}</h2>
+            </div>
+        </div>
+    )
+}
