7ecee521b9a61a167c59d62de2842b2f29b26bdc
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-18 18:02:18 +0200

    notes-app

--- section-4/notes-app/src/App.js
+++ section-4/notes-app/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 import "react-mde/lib/styles/css/react-mde-all.css";
 import './App.css';
+import MoveElement from './utils'
 
 function App() {
   const [notes, setNotes] = React.useState(
     JSON.parse(localStorage.getItem("notes")) || []
   )
 
-  const [currentNoteId, setCurrentNoteId] = React.useState(
-    (notes[0] && notes[0].id) || ""
-  )
+  // setting initial state value with a function to stop it from running on every re-render.
+  const [currentNoteId, setCurrentNoteId] = React.useState(() => (notes[0] && notes[0].id) || "")
 
   React.useEffect(() => {
       localStorage.setItem("notes", JSON.stringify(notes))
   }, [notes])
-  
+
   function createNewNote() {
     const newNote = {
       id: nanoid(),
@@ -34,8 +33,17 @@ function App() {
       return oldNote.id === currentNoteId
       ? { ...oldNote, body: text }
       : oldNote
-    }))
+    }));
+
+    // sort notes by time edited
+    const fromIndex = notes.findIndex(obj => obj.id === currentNoteId);
+    setNotes(prevNotes => MoveElement(prevNotes, fromIndex, 0));
   }
+
+  function deleteNote(event, noteId) {
+    event.stopPropagation()
+    setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+}
   
   function findCurrentNote() {
     return notes.find(note => {
@@ -44,43 +52,45 @@ function App() {
   }
 
   return (
-         <main>
-        {
-          notes.length > 0 
-          ?
-          <Split 
-            sizes={[30, 70]} 
-            direction="horizontal" 
-            className="split"
-          >
-            <Sidebar
-              notes={notes}
-              currentNote={findCurrentNote()}
-              setCurrentNoteId={setCurrentNoteId}
-              newNote={createNewNote}
-            />
-            {
-              currentNoteId && 
-              notes.length > 0 &&
-              <Editor 
-              currentNote={findCurrentNote()} 
-              updateNote={updateNote} 
-              />
-            }
-          </Split>
-          :
-          <div className="no-notes">
-            <h1>You have no notes</h1>
-            <button 
-              className="first-note" 
-              onClick={createNewNote}
-            >
-              Create one now
-            </button>
-          </div>
+    <main>
+      {
+        notes.length > 0 
+        ?
+        <Split 
+          sizes={[30, 70]} 
+          direction="horizontal" 
+          className="split"
+        >
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
           
-        }
-      </main>
+          {
+            currentNoteId && 
+            notes.length > 0 &&
+            <Editor 
+            currentNote={findCurrentNote()} 
+            updateNote={updateNote} 
+            />
+          }
+        </Split>
+        :
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button 
+            className="first-note" 
+            onClick={createNewNote}
+          >
+            Create new note
+          </button>
+        </div>
+        
+      }
+    </main>
   );
 }
 
--- section-4/notes-app/src/components/Sidebar.js
+++ section-4/notes-app/src/components/Sidebar.js
@@ -4,13 +4,15 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split('\n')[0].replace(/[^a-zA-Z 0-9]/g, "")}</h4>
+                <div className="delete-btn" onClick={(e) => props.deleteNote(e, note.id) }>
+                    <div>Delete</div>
+                </div>
             </div>
         </div>
     ))
