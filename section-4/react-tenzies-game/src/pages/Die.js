f09c01f96df466c12ba57b5efea9744bdd6b4fd8
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-20 16:02:04 +0200

    completed tenzies game

--- section-4/react-tenzies-game/src/pages/Die.js
+++ section-4/react-tenzies-game/src/pages/Die.js
@@ -1,7 +1,7 @@
 export default function Die(props) {
     return (
         <div 
-            className={props.isHeld ? "dies--die held": "dies--die"}
+            className={props.isHeld ? "dies--die held": "dies--die rotate-scale-up"}
             onClick={props.holdDice}
         >
             {props.value}
--- section-4/react-tenzies-game/src/pages/index.js
+++ section-4/react-tenzies-game/src/pages/index.js
@@ -2,22 +2,40 @@ import Head from 'next/head'
 import Die from './Die'
 import { useState, useEffect, use } from 'react'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function Home() {
 
-  const allNewDice = () => {
-    return Array.from({ length: 10 }).map(() => {
-    	return {
-      	value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      };
-    });
-  }
-
-  const [dice, setDice] = useState([])
+  const [dice, setDice] = useState([{value: 1, isHeld:false, id: nanoid()}])
+  const [tenzies, setTenzies] = useState(false)
 
   useEffect(() => setDice(() => allNewDice()), [])
+
+  useEffect(() => {
+    // Check if all objects have isHeld: true
+    const allHeld = dice.every(die => die.isHeld);
+
+    // Check if all objects have the same value
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every(die => die.value === firstValue);
+
+    if(allHeld && allSameValue) {
+      setTenzies(true);
+      console.log('winner');
+    }
+  }, [dice])
+
+  const newDie = () => {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    };
+  }
+
+  const allNewDice = () => {
+    return Array.from({ length: 10 }).map(() => newDie());
+  }
 
   const diceElements = dice.map(die => (
     <Die 
@@ -29,17 +47,36 @@ export default function Home() {
   ))
 
   const rollDice = () => {
-    setDice(allNewDice())
-    console.log(dice);
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld
+      ? die
+      : newDie()
+    }))
   }
 
   const holdDice = id => {
-    setDice(prevDice => prevDice.map(prevDice => {
-      return prevDice.id === id
-      ? { ...prevDice, isHeld: !prevDice.isHeld }
-      : prevDice
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id
+      ? { ...die, isHeld: !die.isHeld }
+      : die
     }))
   }
+
+  const newGame = () => {
+    setDice(() => allNewDice())
+    setTenzies(false)
+  }
+
+  // roll with space key
+  const handleSpaceKeyPress = (event) => {
+    if (event.code === 'Space') {
+      rollDice()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleSpaceKeyPress);
+  }, [])
 
   return (
     <>
@@ -49,11 +86,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='container'>
+      <main className='container' onKeyDown={handleSpaceKeyPress}>
+        <div className='title-wrapper'>
+          <h1 className='title'>Tenzies Game</h1>
+          <p className='instructions'>Roll until all dice are equal. Click to hold.</p>
+        </div>
         <div className='dies'>
           {diceElements}
         </div>
-        <button className='roll-dice-btn' onClick={rollDice}>Roll</button>
+        <button className={tenzies ? 'roll-dice-btn blue' : 'roll-dice-btn'} onClick={tenzies ? newGame : rollDice}>{tenzies ? 'New game' : 'Roll'}</button>
+        {tenzies && <Confetti />}
       </main>
     </>
   )
